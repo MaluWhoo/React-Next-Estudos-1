b4f970adc861ca7d17bb7d442e90c71865f129b7
Maria Luiza Lima <luizagamesl90@gmail.com>
2026-01-04 16:50:51 -0300

    Integrando código JSX

--- app/fundamentos/01_integracao/page.tsx
+++ app/fundamentos/01_integracao/page.tsx
@@ -1,7 +1,36 @@
 export default function Page(){
+
+    const regra = <h1>Maior de idade</h1>
+    const nome = "João"
+
+    const lista = [
+        <h1>Anna</h1>,
+        <h1>João</h1>,
+        <h1>Maria</h1>
+    ]
+
+    let salario = 1000
+    let bonus = 200
+
+    // Objeto
+    const usuario = {
+        nome: "Anna",
+        idade: "25",
+        cidade: "São Paulo"
+    }
+
+    function MeuBotao(){
+        return (
+            <button>Clique</button>
+        )
+    }
+
     return (
-        <div>
-            <h1>Integração</h1>
+        <div className={nome}>
+            Olá, {lista[2]}
+            O salário é: {salario + bonus}
+            O usuario mora: {usuario.cidade}, e tem {usuario.idade} anos.
+            {MeuBotao()}
         </div>
     )
 }
