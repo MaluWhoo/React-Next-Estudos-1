77c7fa5628201c84216015d3efa8879833b4cef7
Maria Luiza Lima <luizagamesl90@gmail.com>
2026-01-05 15:55:26 -0300

    Add pages

---
+++ pages/compras.tsx
@@ -0,0 +1,10 @@
+import { Menu } from "@/components/interface";
+
+export default function Page() {
+    return (
+        <div>
+            {Menu()}
+            <h1>Compras</h1>
+        </div>
+    )
+}
